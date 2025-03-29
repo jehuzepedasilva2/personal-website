792397cc4e14f0cb778f52c3ed2ff78c2cd04916
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-03-29 02:30:07 -0700

    moved all svgs into svgs.jsx file

--- src/data.js
+++ src/data.js
@@ -10,7 +10,10 @@ const personalInfo = {
   name: 'Jehu Zepeda-Silva', 
   title: 'Software Engineer', 
   statement: 'Creating innovative and scalable solutions through technology', 
-  about: "I am a recent graduate from UC Berkeley with a Bachelor's in Computer Science, where I cultivated a passion for problem-solving, innovation, and lifelong learning. Outside the world of coding and technology, fitness is an essential part of my routine, helping me stay focused and energized. I also love playing guitar. When I'm not working on personal projects or pursuing my professional goals, I spend quality time with my two dogs, Yarpi and Yukka. Yarpi is a charming Pug-Shih Tzu mix, while Yukka is a Pomeranian. I am driven by curiosity and the desire to learn, grow, and make a meaningful impact. Whether it's taking on challenging projects, exploring new technologies, or finding creative solutions, I embrace every opportunity to push boundaries and expand my horizons.",
+  about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+          Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
   email: 'jehuzepeda@yahoo.com',
   phone: '408-807-5334',
   location: 'Los Gatos, California 95033',
