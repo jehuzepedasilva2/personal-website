828a8ad421f71dce55e2af4450751bcb72c71302
Jehu Zepeda-Silva <jehuzepeda@yahoo.com>
2025-03-30 16:32:05 -0700

    added after hover to glitch effect

--- src/glitchText.js
+++ src/glitchText.js
@@ -1,5 +1,6 @@
 export default function glitchText(textRef) {
   textRef.current.classList.add('glitch-button-text');
+  textRef.current.classList.add('after-hover');
   setTimeout(() => {
     textRef.current.classList.remove('glitch-button-text');
   }, 400);
